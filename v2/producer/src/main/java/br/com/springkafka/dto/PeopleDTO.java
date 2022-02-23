fd858999ca80b793f32c1c050e4f5c54232dc855
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-02-23 19:18:25 -0300

    Separando Produtor e Consumdor

---
+++ v2/producer/src/main/java/br/com/springkafka/dto/PeopleDTO.java
@@ -0,0 +1,15 @@
+package br.com.springkafka.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PeopleDTO {
+
+    private String name;
+    private String cpf;
+
+    private List<String> books;
+
+}
