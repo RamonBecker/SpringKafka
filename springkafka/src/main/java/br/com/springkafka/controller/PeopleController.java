e8f8f13addcb9667d94fb75d7a1bb23458e3fcdf
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-02-23 18:39:03 -0300

    Criando consumidor

--- springkafka/src/main/java/br/com/springkafka/controller/PeopleController.java
+++ springkafka/src/main/java/br/com/springkafka/controller/PeopleController.java
@@ -5,8 +5,10 @@ import br.com.springkafka.dto.PeopleDTO;
 import br.com.springkafka.producer.PeopleProducer;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,6 +22,7 @@ public class PeopleController {
 
     private final PeopleProducer peopleProducer;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> sendMessage(@RequestBody PeopleDTO peopleDTO){
         var id = UUID.randomUUID().toString();
 
